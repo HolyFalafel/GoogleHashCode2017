5204708553234e4a439cdd5ceac08e07d739128f
HolyFalafel <dannysem@gmail.com>
2017-02-23 20:22:14 +0200

    first push

---
+++ FileParser.py
@@ -0,0 +1,17 @@
+filename = 'me_at_the_zoo.in'
+
+with open(filename, 'r') as ro:
+      lines = ro.readlines()
+      # for line in ro.readlines():
+      # first_line = lines[0].strip().split()
+      Videos, Endpoints, Request_Descriptions, Caches, Cache_Size = lines[0].strip().split()
+
+      f = 0
+      # ls = line.strip().split()
+
+
+
+
+
+
+          # rw.write('\t'.join(ls) + '\n')
