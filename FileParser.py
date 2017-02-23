3ed7e9feabbe84ade2e65cc014e0a4dc48bdf787
HolyFalafel <dannysem@gmail.com>
2017-02-23 20:54:48 +0200

    starting to read endpoints

--- FileParser.py
+++ FileParser.py
@@ -2,11 +2,33 @@ filename = 'me_at_the_zoo.in'
 
 with open(filename, 'r') as ro:
       lines = ro.readlines()
-      # for line in ro.readlines():
-      # first_line = lines[0].strip().split()
-      Videos, Endpoints, Request_Descriptions, Caches, Cache_Size = lines[0].strip().split()
+# for line in ro.readlines():
+# first_line = lines[0].strip().split()
+Videos, Endpoints, Request_Descriptions, Caches, Max_Cache_Size = lines[0].strip().split()
 
-      f = 0
+video_sizes = lines[1].strip().split()
+
+lines.remove(lines[0])
+lines.remove(lines[0])
+
+# reading endpoints data
+list_index = 0
+
+Endpoints = int(Endpoints)
+
+endpoint_dict = {}
+
+for endpoint_num in range(0, Endpoints):
+    # reading endpoint line
+    endpoint_latency, num_of_connections = lines[list_index].strip().split()
+    list_index += 1
+
+    endpoint_caches = []
+    # reading all endpoint caches
+    for cache_num in range(0,int(num_of_connections)):
+        bla = 0
+
+f = 0
       # ls = line.strip().split()
 
 
